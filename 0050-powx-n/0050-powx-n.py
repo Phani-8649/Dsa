99aec4626676cabf16456323d943347c1c896bd5
Phani-8649 <120397247+Phani-8649@users.noreply.github.com>
2023-10-06 18:40:37 +0530

    Time: 14 ms (63.22%), Space: 13.1 MB (79.26%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if(n==0):
+            return 1
+        else:
+            return x**n
