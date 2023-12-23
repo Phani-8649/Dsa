ab9cdf467729a9428e9dcdbb9d804b514f0715e7
Phani-8649 <120397247+Phani-8649@users.noreply.github.com>
2023-12-23 11:38:39 +0530

    Time: 19 ms (14.86%), Space: 13.3 MB (45.75%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.py
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -4,9 +4,4 @@ class Solution(object):
         :type celsius: float
         :rtype: List[float]
         """
-        a=[]
-        k=float(celsius)+273.15
-        f=float(celsius)*1.80+32.00
-        a.append(k)
-        a.append(f)
-        return a
+        return [celsius+273.15,celsius*1.80+32.00]
