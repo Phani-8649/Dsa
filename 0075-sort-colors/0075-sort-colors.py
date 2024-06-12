1b41cb9e1f350a5d0958d0b6d35ccd248af89145
Phani-8649 <120397247+Phani-8649@users.noreply.github.com>
2024-06-12 16:08:32 +0530

    Time: 18 ms (46.91%), Space: 11.6 MB (67.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,4 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        for i in range(len(nums)):
+            for j in range(i,len(nums)):
+                if(nums[i]>nums[j]):
+                    t=nums[i]
+                    nums[i]=nums[j]
+                    nums[j]=t
+        return nums
